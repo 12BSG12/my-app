f21a9b441a4826e5ade130cb7db7c107751eba34
12BSG12 <carlj8335@gmail.com>
2022-08-11 18:03:39 +0500

    fixed UsersContainer

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,62 +1,32 @@
-import * as axios from 'axios';
 import User from './User';
 import style from './Users.module.css';
 import defaultAvatar from '../../assets/images/default_avatar.webp';
-import {Component} from 'react';
 
-class Users extends Component { 
-  componentDidMount(){
-    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response =>{
-      this.props.setUsers(response.data.items)
-      this.props.setTotalCount(response.data.totalCount)
-    });
-  };
-  addUsers = () => {
-  }
-  changePage = (page) => {
-    this.props.setCurrentPage(page);
-    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${this.props.pageSize}`).then(response =>{
-      this.props.setUsers(response.data.items)
-    });
-  }
-  
-  render(){
-    let pagesCount = Math.ceil(this.props.totalCount / this.props.pageSize);
-    let pagesArray = [];
-    for (let i = 1; i <= pagesCount; i++) {
-      pagesArray.push(i);
-    }
-    //Эффект карусели
-    let curP = this.props.currentPage;
-    let curPF = ((curP - 5) < 0) ?  0  : curP - 5;
-    let curPL = curP + 4;
-    let slicedPages = pagesArray.slice( curPF, curPL);
-
-    const getUsers = this.props.usersData.map(user => <User 
-      id={user.id}
-      photos={user.photos.small??defaultAvatar}
-      name={user.name} 
-      key={user.id} 
-      // counrty={user.location.counrty}
-      // city={user.location.city}
-      status={user.status}
-      followed ={user.followed}
-      follow={this.props.follow}
-      unFollow={this.props.unFollow}
-    />);
-    return(
-      <div className={style.body}>
-        <div className={style.title}>Users</div>
-        <div className={style.pages}>
-          {slicedPages.map(p => <span onClick={() => this.changePage(p)} className={this.props.currentPage === p && style.p}>{p}</span>)}
-        </div>
-        <div className={style.users}>
-          {getUsers}
-        </div>
-        <button className={style.bigbtn} onClick={this.addUsers}>Show more</button>
+const Users = (props) => {
+  const getUsers = props.usersData.map(user => <User 
+    id={user.id}
+    photos={user.photos.small??defaultAvatar}
+    name={user.name} 
+    key={user.id} 
+    //counrty={user.location.counrty}
+    //city={user.location.city}
+    status={user.status}
+    followed ={user.followed}
+    follow={props.follow}
+    unFollow={props.unFollow}
+  />);
+  return(
+    <div className={style.body}>
+      <div className={style.title}>Users</div>
+      <div className={style.pages}>
+        {props.slicedPages.map(p => <span onClick={() => props.changePage(p)} className={props.currentPage === p ? style.p : ''}>{p}</span>)}
       </div>
-    )
-  }
+      <div className={style.users}>
+        {getUsers}
+      </div>
+      <button className={style.bigbtn}>Show more</button>
+    </div>
+  )
 } 
 
 export default Users;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,13 +1,52 @@
+import * as axios from 'axios';
 import Users from './Users';
+import {Component} from 'react';
 import { connect } from 'react-redux'
-import {
-  followActionCreator, 
+import { followActionCreator, 
   unFollowActionCreator, 
   setUsersActionCreator,
   setPageSizeActionCreator, 
   setTotalCountActionCreator,
   setCurrentPageActionCreator } from '../../redux/users-reducer';
 
+class UsersContainer extends Component { 
+  componentDidMount(){
+    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response =>{
+      this.props.setUsers(response.data.items)
+      this.props.setTotalCount(response.data.totalCount)
+    });
+  };
+
+  changePage = (page) => {
+    this.props.setCurrentPage(page);
+    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${this.props.pageSize}`).then(response =>{
+      this.props.setUsers(response.data.items)
+    });
+  }
+
+  render(){
+    let pagesCount = Math.ceil(this.props.totalCount / this.props.pageSize);
+    let pagesArray = [];
+    for (let i = 1; i <= pagesCount; i++) {
+      pagesArray.push(i);
+    }
+
+    //Эффект карусели
+    let curP = this.props.currentPage;
+    let curPF = ((curP - 5) < 0) ?  0  : curP - 5;
+    let curPL = curP + 4;
+    let slicedPages = pagesArray.slice( curPF, curPL);
+
+    return <Users  
+      usersData={this.props.usersData}
+      follow={this.props.follow}
+      unFollow={this.props.unFollow}
+      slicedPages={slicedPages}
+      changePage={this.changePage}
+      currentPage={this.props.currentPage}
+    />
+  }
+} 
 
 const mapStateToProps = (state) =>({
   usersData: state.usersPage.usersData,
@@ -15,6 +54,7 @@ const mapStateToProps = (state) =>({
   totalCount: state.usersPage.totalCount,
   currentPage: state.usersPage.currentPage
 });
+
 const mapDispatchToProps = (dispatch) =>({
   follow: (id) => {dispatch(followActionCreator(id));},
   unFollow: (id) => {dispatch(unFollowActionCreator(id));},
@@ -23,5 +63,5 @@ const mapDispatchToProps = (dispatch) =>({
   setTotalCount: (count) => {dispatch(setTotalCountActionCreator(count));},
   setCurrentPage: (count) => {dispatch(setCurrentPageActionCreator(count));}
 });
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
-export default UsersContainer;
+
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
