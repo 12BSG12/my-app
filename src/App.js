00e589ac14c4123013f3ef3b338f8216f31f6b7a
12BSG12 <carlj8335@gmail.com>
2022-08-20 20:32:14 +0500

    lazy loading

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import HeaderContainer from './components/Header/HeaderContainer';
 import SidebarContainer from './components/Sidebar/SidebarContainer';
-import ProfileContainer from './components/Profile/ProfileContainer';
-import DialogsContainer from './components/Dialogs/DialogsContainer';
-import News from './components/News/News';
-import Music from './components/Music/Music';
-import Settings from './components/Settings/Settings';
-import UsersContainer from './components/Users/UsersContainer';
-import Login from './components/auth/login';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { Component } from 'react';
 import { connect } from "react-redux";
 import { setInitializedThunkCreator} from './redux/app-reducer';
 import Preloader from './components/common/Preloader/Preloader';
+import {Suspense, lazy} from 'react';
+
+const ProfileContainer = lazy(() => import('./components/Profile/ProfileContainer'));
+const DialogsContainer = lazy(() => import('./components/Dialogs/DialogsContainer'));
+const News = lazy(() => import('./components/News/News'));
+const Music = lazy(() => import('./components/Music/Music'));
+const Settings = lazy(() => import('./components/Settings/Settings'));
+const UsersContainer = lazy(() => import('./components/Users/UsersContainer'));
+const Login = lazy(() => import('./components/auth/login'));
 
 class App extends Component {
   componentDidMount(){
@@ -23,24 +25,24 @@ class App extends Component {
       return <Preloader />
     }
     return (
-      <BrowserRouter>
-        <div className="app-wrapper">
-          <HeaderContainer />
-          <SidebarContainer />
+      <div className="app-wrapper">
+        <HeaderContainer />
+        <SidebarContainer />
+        <Suspense fallback={<Preloader />}>
           <div className='app-wrapper__content'>
             <Routes>
-              <Route path='/profile/:userId' element={<ProfileContainer />}/>
-              <Route path='/profile' element={<ProfileContainer />}/>
-              <Route path='/dialogs/*' element={<DialogsContainer />}/>
-              <Route path='/news' element={<News />}/>
-              <Route path='/music' element={<Music />}/>
-              <Route path='/users' element={<UsersContainer />}/>
-              <Route path='/settings' element={<Settings />}/>
-              <Route path='/login' element={<Login />}/>
-            </Routes>
+                <Route path='/profile/:userId' element={<ProfileContainer />}/>
+                <Route path='/profile' element={<ProfileContainer />}/>
+                <Route path='/dialogs/*' element={<DialogsContainer />}/>
+                <Route path='/news' element={<News />}/>
+                <Route path='/music' element={<Music />}/>
+                <Route path='/users' element={<UsersContainer />}/>
+                <Route path='/settings' element={<Settings />}/>
+                <Route path='/login' element={<Login />}/>
+              </Routes>
           </div>
-        </div>
-      </BrowserRouter>
+        </Suspense>
+      </div>
     );
   }
 }
