8cd0ec474f09da952ebeab590f1842bdc7da365c
12BSG12 <carlj8335@gmail.com>
2022-08-23 21:32:14 +0500

    fixed header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,86 @@
 import style from './Header.module.css';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import {useState} from 'react'
 import { NavLink } from "react-router-dom";
 
 const Header = ({isAuth, login, photo, onClickLogOut}) => {
-  return (
-    <div className={style.header}>
-      <img className={style.logo} src="https://via.placeholder.com/50" alt=''/>
-      <div className={style.loginBlock}>
-        {isAuth ? 
-          (<div>
-          <div>{login}</div>
-          <div className={style.userPhoto}><img src={photo} alt=''/></div>
-          <div className={style.logOut} onClick={onClickLogOut}>logout</div>
-        </div>) : <NavLink className={style.link} to="/login">Login</NavLink>}
-      </div>
-    </div>
-  );
-} 
+  const [anchorElUser, setAnchorElUser] = useState(null);
 
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar position="static" className={style.header}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters sx={{display: {md: 'flex' }, justifyContent: 'space-between'}}>
+          <Typography
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            <img className={style.logo} src="https://via.placeholder.com/50" alt=''/>
+          </Typography>
+          <Box sx={{ flexGrow: 0}}>
+            {
+              isAuth ? <>
+                <Tooltip title={login} >
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }} >
+                    <Avatar alt="" src={photo} />
+                  </IconButton>
+                </Tooltip>
+                <Menu
+                  sx={{ mt: '45px' }}
+                  id="menu-appbar"
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={handleCloseUserMenu}
+                >
+                  <MenuItem onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center" onClick={onClickLogOut}>Logout</Typography>
+                  </MenuItem>
+                </Menu>
+              </>
+              : (<MenuItem onClick={handleCloseUserMenu}>
+                <NavLink to="/login">Login</NavLink>
+              </MenuItem>)
+            }
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
 export default Header;
+
